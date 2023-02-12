5ebe2a45738e4a4dda1a79c77b60a01489e59d00
Mauricio Martinez <n_othing@hotmail.com>
2023-02-12 04:59:00 +0000

    Working

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const page = await browser.newPage()
 const navigationPromise = page.waitForNavigation()
 let logged = false
 
-const scrape = async (user, password, accounts_to_parse) => {
+
+async function scrape(user, password, accounts_to_parse) {
+  let accounts = []
   try {
     if (!(user && password && accounts_to_parse)) {
       console.error('One of the required env vars is not set!')
@@ -13,22 +15,18 @@ const scrape = async (user, password, accounts_to_parse) => {
     }
     accounts_to_parse = accounts_to_parse.map((acc) => `${acc} - MXN`)
 
-    console.log('Navigating to banamex...')
     await page.goto('https://bancanet.banamex.com/MXGCB/JPS/portal/Index.do')
     await page.setViewport({ width: 1280, height: 653 })
 
     try {
-      console.log('Checking for popup...')
       await page.waitForSelector('#splash-207555-close-button', {
         timeout: 3000,
       })
       await page.click('#splash-207555-close-button')
     } catch {
-      console.log('No popup showed up. Proceeding...')
+      // pass
     }
 
-    console.log('Logging in...')
-    console.log('Filling username...')
     await page.waitForSelector('#textCliente')
     await page.focus('#textCliente')
     await page.keyboard.type(user)
@@ -36,17 +34,14 @@ const scrape = async (user, password, accounts_to_parse) => {
 
     await navigationPromise
 
-    console.log('Filling password...')
     await page.waitForSelector('#textFirma')
     await page.focus('#textFirma')
     await page.keyboard.type(password)
     await page.keyboard.press('Enter')
 
-    console.log('Getting in...')
     await navigationPromise
 
     try {
-      console.log('Checking for errors')
       await page.waitForSelector(
         '#modal_commonError > div > div.clear.overflow > div.titulo.modaltitulo > p',
         {
@@ -64,12 +59,11 @@ const scrape = async (user, password, accounts_to_parse) => {
       if (e.message.includes('Solo puedes tener una sesión activa')) {
         throw e
       } else {
-        console.log('No errors found...')
+        // pass
       }
     }
 
     try {
-      console.log('Checking for popup')
       await page.waitForSelector(
         '.overlayofferconatiner > #outerContainer-0 > .outerContainer > .closeSvg > .closeSvgImg',
         {
@@ -80,12 +74,11 @@ const scrape = async (user, password, accounts_to_parse) => {
         '.overlayofferconatiner > #outerContainer-0 > .outerContainer > .closeSvg > .closeSvgImg'
       )
     } catch {
-      console.log('No popup showed up. Proceeding...')
+      // pass
     }
 
     logged = true
 
-    let accounts = []
     await page.waitForSelector('.account-list-content')
     const account_list = await page.$$('.account-list-content')
     for (const element of account_list) {
@@ -105,73 +98,84 @@ const scrape = async (user, password, accounts_to_parse) => {
     }
 
     for (const account of accounts) {
-      console.log(`Retrieving ${account.name} data...`)
       await page.goto(account.link)
       await navigationPromise
       await page.waitForSelector(
-        'body > app-root > app-ada > app-demo-tdc > div.bottom_container > section > div > div.filters.justify-content-between.d-lg-flex > div:nth-child(1) > app-dropdown > div > div.select'
-      )
-      await page.click(
-        'body > app-root > app-ada > app-demo-tdc > div.bottom_container > section > div > div.filters.justify-content-between.d-lg-flex > div:nth-child(1) > app-dropdown > div > div.select'
-      )
-      await page.waitForSelector(
-        'body > app-root > app-ada > app-demo-tdc > div.bottom_container > section > div > div.filters.justify-content-between.d-lg-flex > div:nth-child(1) > app-dropdown > div > div.options.-active.-left.-top > div > div:nth-child(3) > div'
-      )
-      await page.click(
-        'body > app-root > app-ada > app-demo-tdc > div.bottom_container > section > div > div.filters.justify-content-between.d-lg-flex > div:nth-child(1) > app-dropdown > div > div.options.-active.-left.-top > div > div:nth-child(3) > div'
-      )
-      await page.waitForSelector(
-        'body > app-root > app-ada > app-demo-tdc > div.ada_section > div > app-ada-tdc > app-account-detail > div > div.info_area > div.table_info > app-row-info:nth-child(4) > div > div.text_right.label-01 > div'
-      )
-      const no_interests_amount = await page.$eval(
-        'body > app-root > app-ada > app-demo-tdc > div.ada_section > div > app-ada-tdc > app-account-detail > div > div.info_area > div.table_info > app-row-info:nth-child(4) > div > div.text_right.label-01 > div',
-        (div) => div.textContent
-      )
-      account.no_interests_amount = Number(
-        no_interests_amount
-          .replace(/\s+/g, '')
-          .replace(/\,/g, '')
-          .replace(/\$/g, '')
+        'table'
       )
 
+      try {
+        await page.waitForSelector(
+          'body > app-root > app-ada > app-demo-tdc > div.ada_section > div > app-ada-tdc > app-account-detail > div > div.info_area > div.table_info > app-row-info:nth-child(4) > div > div.text_right.label-01 > div'
+        )
+        const no_interests_amount = await page.$eval(
+          'body > app-root > app-ada > app-demo-tdc > div.ada_section > div > app-ada-tdc > app-account-detail > div > div.info_area > div.table_info > app-row-info:nth-child(4) > div > div.text_right.label-01 > div',
+          (div) => div.textContent
+        )
+        account.no_interests_amount = Number(
+          no_interests_amount
+            .replace(/\s+/g, '')
+            .replace(/\,/g, '')
+            .replace(/\$/g, '')
+        )
+      } catch {
+        account.no_interests_amount = 0
+      }
+
       account.data = await page.evaluate(() => {
+        function buildDate(date) {
+          const monthEN = {
+            Ene: 'Jan', Abr: 'Apr', Ago: 'Aug', Dic: 'Dec'
+          }
+
+          if (date.includes(Object.keys(monthEN).join(','))) {
+            return date.replace(/Ene|Abr|Ago|Dic/gi, function (matched) {
+              return `${date.split(' ')[0]} ${monthEN[matched]}  ${new Date().getFullYear()}`
+            })
+          }
+
+          return `${date} ${new Date().getFullYear()}`
+        }
+
         const rows = Array.from(document.querySelectorAll('table tr'))
         return Array.from(rows, (row) => {
           const columns = Array.from(
             row.querySelectorAll('td'),
             (column) => column.innerText
           )
-          // The column 3 is filled if the row is a expense
-          if (columns[3]) {
-            const date = columns[0]
-            const amount = Number(
-              columns[3]
-                .replace(/\s+/g, '')
-                .replace(/\,/g, '')
-                .replace(/\$/g, '')
-            )
-            if (!amount) {
-              // Rejected payment
-              return null
-            }
+          if (columns[2]) {
+            if (!columns[2].includes('En proceso') && columns[2].includes('-')) {
+              const date = buildDate(columns[0])
+              const amount = Number(
+                columns[2]
+                  .replace(/\s+/g, '')
+                  .replace(/\-/g, '')
+                  .replace(/\,/g, '')
+                  .replace(/\$/g, '')
+              )
+              if (!amount) {
+                // Rejected payment
+                return null
+              }
 
-            let concept = columns[1]
-            const match = /(.*)\((\d*):(\d*)\)/g.exec(concept)
-            let current_month = null
-            let total_months = null
-            // If match then the expense is recurrent
-            if (match) {
-              concept = match[1]
-              current_month = Number(match[2])
-              total_months = Number(match[3])
-            }
+              let concept = columns[1]
+              const match = /(.*)\((\d*):(\d*)\)/g.exec(concept)
+              let current_month = null
+              let total_months = null
+              // If match then the expense is recurrent
+              if (match) {
+                concept = match[1]
+                current_month = Number(match[2])
+                total_months = Number(match[3])
+              }
 
-            return {
-              date,
-              amount,
-              concept,
-              current_month,
-              total_months,
+              return {
+                date,
+                amount,
+                concept,
+                current_month,
+                total_months,
+              }
             }
           }
         }).filter((x) => x)
@@ -182,14 +186,9 @@ const scrape = async (user, password, accounts_to_parse) => {
         return accumulator + element.amount
       }, 0)
     }
-
-    console.log(JSON.stringify(accounts))
-  } catch (err) {
-    console.log(`❌ Error: ${err.message}`)
   } finally {
     // Logout
     if (logged) {
-      console.log('Log out...')
       await page.waitForSelector(
         '.bluehead > .blue-top-container > .logout-container > .headerlogout > .logoutlabel'
       )
@@ -202,6 +201,8 @@ const scrape = async (user, password, accounts_to_parse) => {
 
     await browser.close()
   }
+  console.log(accounts)
+  return accounts
 }
 
 export default scrape
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import scrape from './index.js'
 
-await scrape(
+console.log(JSON.stringify(await scrape(
   process.env['CLIENT_NUMBER'],
   process.env['CLIENT_PASSWORD'], 
   JSON.parse(process.env['ACCOUNTS_TO_PARSE'])
-)
+)))
